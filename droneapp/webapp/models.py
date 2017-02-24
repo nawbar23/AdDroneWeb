3076392f7bb0c5092d49f4c2a6b88a60dd3814c5
Pablo <wolter999@gmail.com>
2017-02-24 12:00:55 +0100

    add  model

--- droneapp/webapp/models.py
+++ droneapp/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Drone_info(models.Model):
+    controller_state = models.CharField(max_length=30)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    altitude = models.FloatField()
+    velocity = models.FloatField()
+    batery = models.FloatField()
+    timestamp = models.DateTimeField()
+    id_fly_history = models.ForeignKey('Drone_info', on_delete=models.CASCADE)
+
+class Fly_history(models.Model):
+    time_start = models.DateTimeField()
+    time_end = models.DateTimeField(null=True)
